48f1f7ea8bfd89a30965bd41b43556970c3b6a6f
gouravthakur39 <gourav.thakur39@gmail.com>
2019-09-06 14:10:06 +0530

    add messages resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,12 @@ User.create(username: "Sourav", password: "password")
 User.create(username: "Sonia", password: "password")
 User.create(username: "Manisha", password: "password")
 User.create(username: "Chintu", password: "password")
+
+
+Message.create(body: "Test message from 1", user: User.find(1))
+Message.create(body: "Test message from 2 again ", user: User.find(2))
+Message.create(body: "Test message 3", user: User.find(3))
+Message.create(body: "Test message 4", user: User.find(4))
+Message.create(body: "Test message 5", user: User.find(5))
+Message.create(body: "Test message 6", user: User.find(6))
+Message.create(body: "Test message 7", user: User.find(7))
