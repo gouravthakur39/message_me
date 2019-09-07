57519e559416c7ca362d9d42b591a1a09f9af7be
gouravthakur39 <gourav.thakur39@gmail.com>
2019-09-07 13:31:19 +0530

    modify production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,6 +95,6 @@ Rails.application.configure do
   #config.action_cable.allowed_request_origins = ["https://message-me-gt.herokuapp.com"]
  
   config.action_cable.url = "wss://message-me-gt.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = [ 'https://message-me-gt.herokuapp.com'], /http:\/\/message-me-gt.herokuapp.com.*/
- 
+  #config.action_cable.allowed_request_origins = [ 'https://message-me-gt.herokuapp.com'], /http:\/\/message-me-gt.herokuapp.com.*/
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 end
