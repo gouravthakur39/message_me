30d583060bb9fbe78d1382a7f8cf2c86f6eab7b1
gouravthakur39 <gourav.thakur39@gmail.com>
2019-09-07 12:17:50 +0530

    heroku deployment fix attempt 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.middleware.use ChatActionCable  
+  
   config.web_socket_server_url = "wss://message-me-gt.herokuapp.com/"
  
 end
